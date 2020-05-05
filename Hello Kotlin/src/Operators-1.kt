469d3cbb34c1c03164f69565fc883f863fc369d9
Manan Arora <31929934+manan0313@users.noreply.github.com>
2020-05-06 03:25:57 +0530

    added operator-1.kt

---
+++ Hello Kotlin/src/Operators-1.kt
@@ -0,0 +1,12 @@
+// These are the codes written in REPL mode
+//1+1
+//res0:kotlin.Int = 2
+
+//53-3
+//res1:kotlin.Int = 50
+
+//50/10
+//res2:kotlin.Int = 5
+
+//50.0/10.0
+//res3:kotlin.Double = 5.0
