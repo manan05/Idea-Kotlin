8afec0b4eec80205bd1dbcf5658ab618516c84fc
Manan Arora <31929934+manan0313@users.noreply.github.com>
2020-05-07 23:56:23 +0530

    Quiz

--- 6.Quiz-PracticeTime.kt
+++ 6.Quiz-PracticeTime.kt
@@ -1,3 +1,9 @@
+public fun main(){
+//    basicOps()
+//    vars()
+    nullList()
+}
+
 //Practice Time: Basic Operations
 //Solve the following using the operator methods in one line of code.
 //
@@ -6,8 +12,49 @@
 //swallowed by a hungry moray eel, how many fish do you have left? How many
 //aquariums do you need if you can put 30 fish per aquarium?
 
-public fun main(){
+public fun basicOps(){
     var fish = 2
     var fishLeft = fish.plus(71).plus(233).minus(13).div(30).plus(1)
     println(fishLeft)
+}
+
+//Practice Time: Variables
+//Create a String variable rainbowColor, set its color value, then change it.
+//Create a variable blackColor whose value cannot be changed once assigned. Try changing it anyway.
+
+public fun vars(){
+    var rainbowColor = "blue"
+    println(rainbowColor)
+    rainbowColor = "red"
+    println(rainbowColor)
+
+    val blackColor = "black"
+//    blackColor = "white" //because val cannot be changed
+    println(blackColor)
+
+}
+
+//Practice Time: Nullability
+//Try to set rainbowColor to null.
+//Declare two variables, greenColor and
+//blueColor. Use two different ways of setting them to null.
+
+public fun nullElem() {
+    var greenColor = null
+    var blueColor : Int? = null
+    blueColor = 20
+}
+
+//Practice Time: Nullability/Lists
+//Create a list with two elements that are null; do it in two different ways.
+//Next, create a list where the list is null.
+
+public fun nullList(){
+    var z = listOf(null,null)
+    print(z)
+
+    var list:List<Int?> = listOf(null,null)
+    var list2:List<Int>? = null // using elvis operator to state that this var can be null too
+
+
 }
