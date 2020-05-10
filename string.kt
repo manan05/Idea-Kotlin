4f83fe7e529b03d4eec00e3eb0586d19a3dd40bb
Manan Arora <31929934+manan0313@users.noreply.github.com>
2020-05-11 02:43:03 +0530

    Create string.kt

---
+++ string.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+}
+
+//Normal printing String and defining it
+public fun Hello(){
+    val hello = "Hello Fish"
+    println(hello)
+}
+
+public fun addString(){
+    val hello = "Hello"
+    val fish = "fish"
+    println(hello + fish)
+}
+
+public fun accessNumString(){
+    val noOfFish = 5
+    val noOfPlant = 12
+    println("I have $noOfFish fishes and $noOfPlant plants.")
+}
