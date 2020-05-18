355b4757f27c854841a1eeb261dd2da835e61c22
Manan Arora <31929934+manan0313@users.noreply.github.com>
2020-05-19 05:12:16 +0530

    Create arrays_loops.kt

---
+++ arrays_loops.kt
@@ -0,0 +1,18 @@
+public fun main() {
+    errorMyList()
+    removeFromMyList()
+}
+
+public fun errorMyList() {
+    val myList = mutableListOf("tuna", "salmon", "shark")
+    // there will be an error if we try to change the values of myList
+    // error = val cannot be reassigned
+    println(myList)
+}
+
+public fun removeFromMyList() {
+    val myList = mutableListOf("tuna", "salmon", "shark")
+    println(myList)
+    myList.remove("shark")
+    println(myList)
+}
