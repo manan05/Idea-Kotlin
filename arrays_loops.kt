b3a8329704110873f79415854fa6fea9d1b13a2c
manan05 <mananarora313@gmail.com>
2020-05-20 02:31:28 +0530

    added useListOf() function

--- arrays_loops.kt
+++ arrays_loops.kt
@@ -1,8 +1,9 @@
 public fun main() {
     errorMyList()
-//    removeFromMyList()
+    removeFromMyList()
+    useListOf()
 }
-
+// This function shows an error that we get while modifying value of a list
 public fun errorMyList() {
     val myList = mutableListOf("tuna", "salmon", "shark")
     // there will be an error if we try to change the values of myList
@@ -10,9 +11,18 @@ public fun errorMyList() {
     println(myList)
 }
 
+// This function removes the elem from the list
 public fun removeFromMyList() {
     val myList = mutableListOf("tuna", "salmon", "shark")
     println(myList)
     myList.remove("shark")
     println(myList)
+}
+
+// creates a list of elements using the function listOf()
+public fun useListOf(){
+    val fish = 12
+    val plants = 5
+    val swarm = listOf(fish,plants)
+    println(swarm)
 }
