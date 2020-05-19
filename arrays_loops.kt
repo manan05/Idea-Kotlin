943e87cfa76848fa376c418cb126c34554b3285d
manan05 <mananarora313@gmail.com>
2020-05-20 02:57:18 +0530

    Added makeMixStrings() fun

--- arrays_loops.kt
+++ arrays_loops.kt
@@ -1,7 +1,10 @@
+import java.util.*
+
 public fun main() {
     errorMyList()
     removeFromMyList()
     useListOf()
+    makeMixStrings()
 }
 // This function shows an error that we get while modifying value of a list
 public fun errorMyList() {
@@ -25,4 +28,19 @@ public fun useListOf(){
     val plants = 5
     val swarm = listOf(fish,plants)
     println(swarm)
+}
+
+//
+public fun makeMixStrings(){
+    // make strings of only integers
+    val numbers = intArrayOf(10,20,30,40)
+    println(Arrays.toString(numbers))
+
+
+//    val thisArray = Arrays.toString(intArrayOf(2,"foo")) // this line gives type mismatch error
+//    println(thisArray)
+
+    // a mix array containing strings, int, double, char
+    val mix = arrayOf("fish" , 2 , 3.15 , 'a')
+    println(Arrays.toString(mix))
 }
